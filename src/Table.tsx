a00ef63233512ce184cd1b0577cea545b4aa61e2
James <james.d.langridge@gmail.com>
2023-04-18 10:12:44 +0200

    Remove edit button

--- src/Table.tsx
+++ src/Table.tsx
@@ -50,10 +50,6 @@ export default function Table() {
                             </th>
                           );
                         })}
-
-                        <th scope="col" className="relative py-3.5 px-4">
-                          <span className="sr-only">Edit</span>
-                        </th>
                       </tr>
                     </thead>
 
@@ -75,25 +71,6 @@ export default function Table() {
                                 </td>
                               );
                             })}
-
-                            <td className="px-4 py-4 text-sm whitespace-nowrap">
-                              <button className="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg dark:text-gray-300 hover:bg-gray-100">
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                  strokeWidth="1.5"
-                                  stroke="currentColor"
-                                  className="w-6 h-6"
-                                >
-                                  <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
-                                  />
-                                </svg>
-                              </button>
-                            </td>
                           </tr>
                         );
                       })}
